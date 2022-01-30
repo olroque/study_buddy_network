84f64f66b795d717a3307cb3e28c078b174cc9f7
olroque <username@example.com>
2022-01-30 14:16:33 -0500

    updated settings again

--- studybud/settings.py
+++ studybud/settings.py
@@ -10,12 +10,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y(%&s)dl7jmrkb7&u9*&1!a2hi_9@1ou2(#69v=x!xci_2y4&5'
+SECRET_KEY = os.environ.get('SECRET_KEY_STUDY_BUDDY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['studybuddynetwork.herokuapp.com']
 
 
 # Application definition
